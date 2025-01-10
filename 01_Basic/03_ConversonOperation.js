88a3a223e576d40550c61794b8ca0fcd0d59ec66
saurabh kumar <saurabhslite2@gmail.com>
2025-01-10 14:42:57 +0530

    Operations

--- 01_Basic/03_ConversonOperation.js
+++ 01_Basic/03_ConversonOperation.js
@@ -26,7 +26,52 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+
+// ************************ Operations *************************
+
+let value =3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "hello "
+let str2 = "saurabh"
+let str3 = str1+ str2
+
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1"+"2");
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log("1"+ 2 + "2");
+// console.log(1 + 2 + "2");
+// console.log(1 + "2" + "2");
+// console.log(3 + 4 * 5 % 6)
+
+// console.log(+true);
+
+
+num1 = num2 = num3 = 2 + 2
+
+// prefix and postfix
+
+let gameCounter = 100
+++gameCounter //gameCounter++
+//console.log(gameCounter);
+
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+//https://tc39.es/ecma262/#sec-abstract-operations
+
+
+
+
+
+
 
 
