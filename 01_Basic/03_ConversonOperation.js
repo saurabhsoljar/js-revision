1d5206ec68c757d9cd7175a837779f8c0d74b619
saurabh kumar <saurabhslite2@gmail.com>
2025-01-10 12:49:52 +0530

    ConversionOperation

---
+++ 01_Basic/03_ConversonOperation.js
@@ -0,0 +1,32 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)
+// console.log(typeof  valueInNumber)
+// console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false=>0
+
+/*let isLoggdIn = 0
+
+let booleanIsLoggedIn= Boolean(isLoggdIn)
+console.log(booleanIsLoggedIn);
+
+*/
+
+//1 => true, 0=> false
+//" " => false
+//"saurabh"=> true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
