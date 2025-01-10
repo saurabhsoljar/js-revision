176979b009caeb8d91f4c2e4e9009b3d65d6fe2b
saurabh kumar <saurabhslite2@gmail.com>
2025-01-10 15:20:33 +0530

    DataType

---
+++ DataTyoes-Summary.js
@@ -0,0 +1,21 @@
+// Primitive 
+
+// 7 Type : String , Number, Boolearn, null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const aotherId = Symbol('123')
+console.log(id === aotherId);
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
