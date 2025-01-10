eafd95b90a4b3f3b5d60fbe5d39c26aaf4b00c02
saurabh kumar <saurabhslite2@gmail.com>
2025-01-10 12:27:37 +0530

    DataTypes

---
+++ 01_Basic/02_DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+//alert (3+3) we are using nodejs, not browser
+
+console.log(3+3);
+console.log("saurabh");
+
+//code redability should be high
+
+let name = "saurabh"
+let agr = 24
+let isLoggedIn = false
+
+//number = 2 to power 53
+// bigint
+//string = ""
+//boolean = true/false
+//null = is type/standalone value
+//undefined = 
+//symbol = unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+
