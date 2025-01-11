86962429bad7f923e7acc23050ae58244bf5a11e
saurabh kumar <saurabhslite2@gmail.com>
2025-01-11 10:53:14 +0530

    Date & time

---
+++ 01_Basic/07_Datesin.js
@@ -0,0 +1,10 @@
+// date 
+
+let myDate  = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+
+
+
