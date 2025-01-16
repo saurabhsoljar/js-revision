e944aeafd6d15c34d1765ec6f6056ffbc1841bba
saurabh kumar <saurabhslite2@gmail.com>
2025-01-16 12:53:08 +0530

    scope

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -3,10 +3,40 @@ let a = 10
 const b = 20 
 var c = 30 
 
-{}
+if (true){
+  let a = 10
+  const b= 20
+  //console.log("INNER:", a);
+  
+}
 
-console.log(a);
-console.log(b);
-console.log(c);
+//console.log(a);
+// console.log(b);
+// console.log(c);
 
+function one () {
+  const username = "saurabh"
+
+  function two(){
+    const website = "youTube"
+    console.log(username);
+  }
+  //console.log(website);
+  two()
+}
+
+//one()
+
+if (true) {
+  const username = "saurabh"
+  if(username === "saurabh"){
+    const website = " youtub"
+    console.log(username + website);
+  }
+  //console.log(website);
+  
+}
+//console.log(username);
+
+//+++++++++++++++++++++++++++++ interesting   +++++++++++++
 
