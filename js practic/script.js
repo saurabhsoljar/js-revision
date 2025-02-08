3e773b7c0a5501c0ea0075ae1eb7d24dce0e5f76
saurabh kumar <saurabhslite2@gmail.com>
2025-02-08 21:31:25 +0530

    js interview question practic

---
+++ js practic/script.js
@@ -0,0 +1,138 @@
+//1.
+// console.log("saurabh","my favorite hobby to play cricket")
+//2
+//console.log(45*2-10);
+
+//3
+// const sk = new Date();
+// sk.getFullYear()
+// console.log(sk.getFullYear());
+
+//4
+// var fast="saurabh";
+// var last="kushwasha";
+// console.log(fast+ " " +last);
+// console.log(`${fast} ${last}`);
+
+//5
+// let a=20;
+// console.log(a);
+// a=25;
+// console.log(a);
+
+//6
+// console.error("somthing is wrong")
+
+//7
+//console.log(12*12);
+
+//8
+// var a="saurabh";
+// console.log(typeof a);
+
+//9
+// let age=17;
+// if (age>18) {
+//   console.log("its grater then 18 ");
+// }else{
+//   console.log(false);
+// }
+
+//10
+// console.log(100/0);
+
+//11
+// let a=10;
+// console.log(a);
+
+//12
+// const pi= Math.PI;
+// console.log(pi);
+
+//13
+// let a="saurabh"
+// a="soljar"
+// console.log(a);
+
+//14
+// console.log(typeof null);
+
+//15
+// let a="25";
+// console.log(typeof a);
+
+//16
+// let a = true;
+// console.log(typeof a);
+
+//17
+// let a="saurabh",  b=567, c=true;
+// console.log(a,b,c);
+
+//18
+// let a;
+// console.log(typeof a);
+
+//19
+// var a=undefined;
+// console.log(typeof a);
+
+//20
+// const array = [];
+// array=[]
+
+//21
+
+// for(let i=1; i<=50; i++){
+//   console.log(i);
+// }
+
+//22
+// let i = 1;
+// while (i <= 50) {
+//   console.log(i);
+//   i++;
+// }
+
+//23
+// let a = 1;
+// let sum = 0;
+
+// while (a <= 10) {  // Correct condition
+//   sum = sum + a;
+//   a++;
+//   console.log(sum);
+// }
+
+// Output: 55
+
+//24
+// let str = "saurabh"
+// for(let character of str){
+//   console.log(character);
+// }
+
+//25
+// for (let i = 1; i < 21; i++) {
+//   if (i % 2 === 0) {
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+//26
+// let a=5;
+// do{
+//   console.log(a);
+//   a--;
+// }
+// while(a>0)
+
+//27
+
+// let fact = 1;
+// for (var i = 5; i > 0; i--) {
+//   fact = fact * i;
+// }
+// console.log(fact);
+
