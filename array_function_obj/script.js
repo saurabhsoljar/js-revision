736a4354710b160b76e6762b39a71f3283d647e2
saurabh kumar <saurabhslite2@gmail.com>
2025-02-12 10:17:00 +0530

    arr,fun,obj

---
+++ array_function_obj/script.js
@@ -0,0 +1,36 @@
+//Array
+var fruts = ["orange", "apple", "mango", "banana", "gava"];
+// console.log(fruts[0]);
+// console.log(fruts.length);
+// fruts.push("papaya");
+// console.log(fruts);
+// fruts.pop("orange")
+// console.log(fruts);
+// fruts.unshift("kela");
+// console.log(fruts);
+// fruts.shift();
+// console.log(fruts);
+
+//function
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(4, 4));
+
+//object
+
+// let person = {
+//   name: "saurabh",
+//   rollno: 567,
+//   age: 25,
+//   grade: ["a", "b", "c"],
+//   great: function () {
+//     console.log(this.name);
+//   },
+// };
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.rollno);
+// console.log(person.grade[1]);
+
